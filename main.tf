0ab00e57546d5ba8f1d59ca75bef8f687c214ac5
kamakell <55107981+kamakell@users.noreply.github.com>
2020-01-21 20:37:49 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,105 @@
+terraform {
+  backend "gcs" {
+    bucket  = "sales-eu01-shared"
+    prefix  = "terraform/state"
+  }
+}
+// Configure the Google Cloud provider
+provider "google" {
+ # credentials = "${file("${var.credentials}")}"
+ project     = "${var.gcp_project}"
+ region      = "${var.region}"
+}
+// Create VPC
+resource "google_compute_network" "vpc" {
+ name                    = "${var.network}-vpc"
+ auto_create_subnetworks = "false"
+}
+
+// Create Subnet
+resource "google_compute_subnetwork" "public" {
+ name          = "${var.network}-public"
+ description   = "This subnet is Public Subnetwork"
+ ip_cidr_range = "${var.public_cidr}"
+ network       = "${var.network}-vpc"
+ depends_on    = [google_compute_network.vpc]
+ region      = "${var.region}"
+ private_ip_google_access = "${var.private_google_access}"
+ }
+
+resource "google_compute_subnetwork" "private" {
+ name          = "${var.network}-private"
+ ip_cidr_range = "${var.secondary_subnet_cidr}"
+ network       = "${var.network}-vpc"
+ depends_on    = [google_compute_network.vpc]
+ region      = "${var.region}"
+}
+// VPC Route Configuration
+// resource "google_compute_route" "route-igw" {
+//   name         = "route-igw"
+//   dest_range   = "${var.igw}"
+//   depends_on    = [google_compute_network.vpc]
+//   network      = "${var.network}-vpc"
+//   next_hop_ip = "${var.hop}"
+//   priority     = "${var.priority}"
+// }
+// VPC firewall configuration
+// resource "google_compute_firewall" "firewall" {
+//   name    = "${var.network}-firewall"
+//   network = "${google_compute_network.vpc.name}"
+
+//   allow {
+//     protocol = "icmp"
+//   }
+
+//   allow {
+//     protocol = "tcp"
+//     ports    = ["22"]
+//   }
+
+//   source_ranges = ["0.0.0.0/0"]
+// }
+
+resource "google_compute_firewall" "ssh" {
+  name    = "${var.network}-firewall-ssh"
+  network      = "${var.network}-vpc"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["${var.network}-firewall-ssh"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "${var.network}-firewall-http"
+  network      = "${var.network}-vpc"
+  enable_logging = "true"
+  priority = "1010"
+  source_ranges = ["0.0.0.0/0"]
+  source_service_accounts = ["hpa01-a@sales-eu01.iam.gserviceaccount.com"]
+   allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_service_accounts  = ["hpa01-a@sales-eu01.iam.gserviceaccount.com"]
+   allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "arjun" {
+  name    = "${var.network}-firewall-arjun"
+  network      = "${var.network}-vpc"
+  enable_logging = "true"
+  priority = "1020"
+  target_tags = ["annanya"]
+  source_tags = ["abc"]
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
